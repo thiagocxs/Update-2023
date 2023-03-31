94f52b7687a3f17cde820ed1f2d5deb67ccf5f04
thiagocxs <88004632+thiagocxs@users.noreply.github.com>
2023-03-31 06:15:53 -0300

    Criação dos métodos GETS ans SETTINGS

--- AppListaCurso/app/src/main/java/br/com/tacheio/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/br/com/tacheio/applistacurso/model/Pessoa.java
@@ -4,6 +4,44 @@ public class Pessoa {
 
     //Construtor
 
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
     public Pessoa(){
 
 
